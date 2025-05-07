217f3b2b2c777b23ac8ea7c9ba752d99d8cdc393
joonwan <111417356+joonwan@users.noreply.github.com>
2025-05-07 23:57:06 +0900

    feat: init project

---
+++ src/main/java/com/canary/all_backtesting/AllBacktestingApplication.java
@@ -0,0 +1,13 @@
+package com.canary.all_backtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllBacktestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllBacktestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canary/all_backtesting/AllBacktestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canary.all_backtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllBacktestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
